14e0627d36d306df118f4a45ab26138c47a39a30
artemkosnikov <Boxberry123>
2021-06-21 12:08:23 +0300

    test fixed

--- src/js/arrayBufferConverter.test.js
+++ src/js/arrayBufferConverter.test.js
@@ -1,10 +1,9 @@
 import { ArrayBufferConverter, getBuffer } from './ArrayBufferConverter';
 
 test('Проверка добавления буффера в загрузчик', () => {
-  const buffData = getBuffer();
+  const expected = '{\"data\":{\"user\":{\"id\":1,\"name\":\"Hitman\",\"level\":10}}}'
   const convert = new ArrayBufferConverter();
-  convert.load(buffData);
-  const result = convert.toString();
+  convert.load(getBuffer());
   /* eslint-disable */
-    expect(result).toBe('{\"data\":{\"user\":{\"id\":1,\"name\":\"Hitman\",\"level\":10}}}');
+    expect(`${convert}`).toBe(expected);
 });
